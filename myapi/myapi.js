90db9fd2e60dda84a7d1b923d70953418a30cb85
AL Musheer <musheerayan@gmail.com>
2025-09-04 19:22:57 +0530

    Delete myapi directory

--- myapi/myapi.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/message", (req, res) => {
-  res.json({ text: "Hello from Render API 🚀" });
-});
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
