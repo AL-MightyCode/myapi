6c67601fe27804c7ef4a874d603a59ab0f7b73bb
AL Musheer <musheerayan@gmail.com>
2025-09-04 19:16:47 +0530

    Add files via upload

---
+++ myapi/myapi.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/message", (req, res) => {
+  res.json({ text: "Hello from Render API 🚀" });
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
